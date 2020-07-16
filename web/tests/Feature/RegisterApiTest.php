1904e4fd37df2aa85638c98eb434396d0e4d73a3
p-t-a-p-1 <kb240225+1@gmail.com>
2020-07-16 23:06:40 +0900

    [add] テストコード追加（ユーザー登録処理）

---
+++ web/tests/Feature/RegisterApiTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RegisterApiTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function should_新しいユーザーを作成して返却する()
+    {
+        $data = [
+            'name' => 'vuesplash user',
+            'email' => 'dummy@email.com',
+            'password' => 'test1234',
+            'password_confirmation' => 'test1234',
+        ];
+
+        $response = $this->json('POST', route('register'), $data);
+
+        // userのnameが一致してるか
+        $user = User::first();
+        $this->assertEquals($data['name'], $user->name);
+
+        // レスポンスのステータスコードが201で 'name'と値のjsonがあるか
+        $response->assertStatus(201)->assertJson(['name' => $user->name]);
+    }
+}
