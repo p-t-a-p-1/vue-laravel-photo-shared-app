6dee8165c9a41a9f7b98596b523d9f17af1c5938
p-t-a-p-1 <kb240225+1@gmail.com>
2020-07-17 09:31:53 +0900

    [add] Laravel7でLogin機能使えるようにライブラリインストール

--- web/routes/web.php
+++ web/routes/web.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/{any?}', function () {
     return view('index');
 })->where('any', '.+');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
--- web/webpack.mix.js
+++ web/webpack.mix.js
@@ -11,9 +11,5 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.browserSync('vuesplash.test').js('resources/js/app.js', 'public/js').version();
-
-mix.browserSync({
-    proxy: '0.0.0.0:8081', // アプリの起動アドレス
-    open: false // ブラウザを自動で開かない
-})
+mix.js('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css');
