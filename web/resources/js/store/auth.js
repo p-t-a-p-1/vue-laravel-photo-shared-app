7e6408f96787e07cbd19881efeb6e3f806b56ed5
p-t-a-p-1 <kb240225+1@gmail.com>
2020-07-25 15:03:01 +0900

    [update] vuexの設定更新

--- web/resources/js/store/auth.js
+++ web/resources/js/store/auth.js
@@ -1,9 +1,24 @@
-const state = {}
+/**
+ * Vuex（状態管理ライブラリ）のストアの設定
+ */
 
+// データの入れ物（ログイン中のユーザーデータ）
+const state = {
+    user: null
+}
+
+// ステートの内容から算出される値（ユーザーがログイン中であるかどうか）
 const getters = {}
 
-const mutations = {}
+// ステートを同期処理で更新するためのメソッド
+const mutations = {
+    setUser (state, user) {
+        state.user = user
+    }
+}
 
+// ステートを非同期処理で更新するためのメソッド
+// APIとの通信などの非同期処理を行った後にミューテーションを呼び出してステートを更新する
 const actions = {}
 
 // 名前空間で分けておく
