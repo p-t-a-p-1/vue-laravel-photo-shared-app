7780a2a799a1e63db642d25ef1f40c7abca4eb2a
p-t-a-p-1 <kb240225+1@gmail.com>
2020-08-11 15:28:39 +0900

    [update] 写真一覧コンポーネントで写真単体コンポーネント呼び出して写真表示させる

--- web/resources/js/pages/PhotoList.vue
+++ web/resources/js/pages/PhotoList.vue
@@ -1,3 +1,51 @@
 <template>
-    <h1>Photo List</h1>
+    <div class="photo-list">
+        <div class="grid">
+            <Photo
+                class="grid__item"
+                v-for="photo in photos"
+                :key="photo.id"
+                :item="photo"
+            />
+        </div>
+    </div>
 </template>
+
+<script>
+import { OK } from '../util'
+import Photo from '../components/Photo.vue'
+
+export default {
+    components: {
+        Photo,
+    },
+    data () {
+        return {
+            photos: []
+        }
+    },
+    methods: {
+        async fetchPhotos () {
+            const response = await axios.get('/api/photos')
+
+            if (response.status !== OK) {
+                this.$store.commit('error/setCode', response.status)
+                return false
+            }
+
+            this.photos = response.data.data
+        }
+    },
+    watch: {
+        // ページが変わった時にfetchPhotosを実行するように
+        $route: {
+            // 監視ハンドラ
+            async handler () {
+                await this.fetchPhotos()
+            },
+            immediate: true // コンポーネントが生成されたタイミングでも実行（2ページ目以降）
+        }
+    }
+}
+
+</script>
