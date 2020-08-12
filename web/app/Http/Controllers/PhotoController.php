d0fe4ec6573d79b2b8cb9bf01058a175b209c335
p-t-a-p-1 <kb240225+1@gmail.com>
2020-08-13 01:39:24 +0900

    [update] コメント投稿のメインロジック実装、写真詳細APIのコントローラーも修正

--- web/app/Http/Controllers/PhotoController.php
+++ web/app/Http/Controllers/PhotoController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\StorePhoto;
 use App\Photo;
+use App\Comment;
+use App\Http\Requests\StorePhoto;
+use App\Http\Requests\StoreComment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
@@ -108,9 +110,28 @@ class PhotoController extends Controller
      */
     public function show(string $id)
     {
-        $photo = Photo::where('id', $id)->with(['owner'])->first();
+        $photo = Photo::where('id', $id)->with(['owner', 'comments.author'])->first();
 
         // 写真データがない場合は404
         return $photo ?? abort(404);
+    }
+
+    /**
+     * コメント投稿
+     * @param Photo $photo
+     * @param StoreComment $request
+     * @return \Illuminate\Http\Response
+     */
+    public function addComment(Photo $photo, StoreComment $request)
+    {
+        $comment = new Comment();
+        $comment->content = $request->get('content');
+        $comment->user_id = Auth::user()->id;
+        $photo->comments()->save($comment);
+
+        // authorリレーションをロードするためにコメントを取得し直す
+        $new_comment = Comment::where('id', $comment->id)->with('author')->first();
+
+        return response($new_comment, 201);
     }
 }
