bfcd44225b9964bfdf57663844d7ec7569e5914d
p-t-a-p-1 <kb240225+1@gmail.com>
2020-08-20 21:50:12 +0900

    [update] CircleCI上で使用するDB情報を追記

--- web/config/database.php
+++ web/config/database.php
@@ -95,7 +95,23 @@ return [
             'driver' => 'sqlite',
             'database' => ':memory',
             'prefix' => '',
-        ]
+        ],
+
+        // Circle CI用
+        'circle_testing' => [
+            'driver' => 'mysql',
+            'host' => '127.0.0.1',
+            'port' => '3306',
+            'database' => 'circle_test',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+        ],
 
     ],
 
